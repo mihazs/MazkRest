bd481bbb15ad7cd7032e0799de611af16f6864ea
mihazs <mihael.zaminsousa@gmail.com>
2015-09-24 12:57:20 -0300

    Método de atualizar usuário implementado.

--- src/main/java/com/mycompany/mazkrest/UsuarioResource.java
+++ src/main/java/com/mycompany/mazkrest/UsuarioResource.java
@@ -17,8 +17,10 @@ import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -81,7 +83,20 @@ public class UsuarioResource {
         //u.setTentativaList(new TentativaDAO().findByUsuario(u));
         return u;
     }
-
+    
+    @POST
+    @Path("update")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Usuario updateUsuario(Usuario u)
+    {
+        StandardDAO sDao = new StandardDAO();
+        sDao.beginTransaction();
+        u = sDao.getEntityManager().merge(u);
+        sDao.commit();
+        sDao.closeTransaction();
+        return u;
+    }
     @GET
     @Path("login")
     @Produces(MediaType.APPLICATION_JSON)
